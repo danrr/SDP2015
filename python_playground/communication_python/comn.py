87cd1e905e2f33176ba3ea2427b07d379deddaf2
Bartek <bartlomiejmalon@hotmail.com>
2015-01-26 16:56:39 +0000

    Bartek Malon
    Added a listener which uses the 'termios' module

--- python_playground/communication_python/comn.py
+++ python_playground/communication_python/comn.py
@@ -2,10 +2,13 @@ import serial
 import sys
 import time
 from threading import Thread
+import termios
+import contextlib
 
 class Communication():
 
-	def __init__(self, port='COM3', baudrate=115200, timeout=5):
+	# REMEMBER TO CHANGE THE PORT FOR WINDOWS/MAC/LINUX
+	def __init__(self, port='/dev/ttyACM0', baudrate=115200, timeout=5):
 		self.serial 	= serial.Serial(port,baudrate,timeout)
 		self.port 		= port
 		self.baudrate 	= baudrate
@@ -39,12 +42,39 @@ class Communication():
 		self.serial.flush()
 		self.serial.close()
 
-if __name__ == '__main__':
-	# Accept input from the user through the command line, for example: "python comn.py 1"
-	comm = Communication()
-	for arg in sys.argv:
-		comm.send(arg)
+@contextlib.contextmanager
+def raw_mode(file):
+    old_attrs = termios.tcgetattr(file.fileno())
+    new_attrs = old_attrs[:]
+    new_attrs[3] = new_attrs[3] & ~(termios.ECHO | termios.ICANON)
+    try:
+        termios.tcsetattr(file.fileno(), termios.TCSADRAIN, new_attrs)
+        yield
+    finally:
+        termios.tcsetattr(file.fileno(), termios.TCSADRAIN, old_attrs)
+
+def main():
+	# Executing python comn.py will start the program (no arguments)
+    print 'exit with ^C or ^D'	
+    comm = Communication()
+
+    with raw_mode(sys.stdin):
+        try:
+            while True:
+                ch = sys.stdin.read(1)
+                if not ch or ch == chr(4):
+                    break
+                char = str.upper(ch)
+                print '%02x' % ord(char)	# prints the hex value of the character (A/B/C/D/E)
+                char += char 				# duplicates the character (must be e.g. AA, not A)
+                print char
+                comm.send(char)
+        except (KeyboardInterrupt, EOFError):
+            pass
 
 	# thread = Thread(target = comm.read, args=[])
 	# thread.start()
 	# thread.join()
+
+if __name__ == '__main__':
+    main()
