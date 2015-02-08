d88760b08338932915ae02c43df1eb1f397e3642
danristea <danraduristea@gmail.com>
2015-02-08 15:40:57 +0000

    introduced a command queue to which stores instructions and time. This allows us to have non-blocking waits.

--- arduino/control/control.ino
+++ arduino/control/control.ino
@@ -14,6 +14,7 @@
 
 byte cmd, data, response;
 Servo kicker;
+unsigned long current_millis;
 
 /* Assign a unique ID to the sensors */
 
@@ -22,8 +23,16 @@ Adafruit_LSM303_Accel_Unified  accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified    mag   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_L3GD20_Unified        gyro = Adafruit_L3GD20_Unified(20);
 
+typedef struct Command {
+  unsigned long millis;
+  byte data;
+  void (*functionPtr)(byte);
+} Command;
+
+Command commands[7];
+
 /*
-  Command class bytes. Set to gaming-related stuff
+  Command class bytes. Set to kgaming-related stuff
   now for debugging purposes, but will change to something
   more professional-looking in the future.
 */
@@ -58,174 +67,12 @@ void setup() {
   kicker.attach(5, 600, 2400); 
   // initialise the IMU
   initSensors();
-}
-
-/*
-  Main loop. Runs an endless loop of fetching commands from the serial
-  comms, decoding them, and generally doing the stuff we're supposed to do.
-*/
-
-void loop() {
-  fetchCommand();
-  decodeCommand();
-  respond();
-}
-
-/*
-  Fetches a single command (command byte + data byte) from
-  the serial stream if there is data available.
-*/
-
-void fetchCommand() {
-  if (Serial.available() >= 2) {
-    cmd = (char)Serial.read();
-    data = (char)Serial.read();
-    response = 0x00;
-  }
-}
-
-void respond() {
-  if (response) {
-    Serial.write(response);
-  }
-  response = 0x00;
-}
-
-/*
-  Move forward with the power specified. Currently, 0<=power<=100,
-  but will probably make more sense to use the whole byte range
-  0-255 in the future.
-*/
-
-void moveForward(byte power) {
-  motorBackward(LEFT_DRIVE, power);
-  motorForward(RIGHT_DRIVE, power);
-}
-
-/*
-  Move backward with the power specified. Currently, 0<=power<=100,
-  but will probably make more sense to use the whole byte range
-  0-255 in the future.
-*/
-
-void moveBackward(byte power) {
-  motorForward(LEFT_DRIVE, power); 
-  motorBackward(RIGHT_DRIVE, power * 1);
-}
-
-void turnLeft(byte power) {
-   motorForward(LEFT_DRIVE, power);
-   motorForward(RIGHT_DRIVE, power);
- }
- 
-void turnRight(byte power) {
-   motorBackward(LEFT_DRIVE, power);
-   motorBackward(RIGHT_DRIVE, power);
- }
-
-void closeGrabber() {
-  cmd = 0x00;
-  motorForward(LEFT_GRABBER, 100);
-  motorBackward(RIGHT_GRABBER, 100);
-  delay(500);
-  motorAllStop();
-  motorStop(LEFT_GRABBER);
-  motorStop(RIGHT_GRABBER);  
-}
-
-void openGrabber() {
-  cmd = 0x00;
-  motorBackward(LEFT_GRABBER, 100);
-  motorForward(RIGHT_GRABBER, 100);
-  delay(500);
-  motorAllStop();
-  motorStop(LEFT_GRABBER);
-  motorStop(RIGHT_GRABBER);  
-}
-
-/*
-  Perform a kick. Takes a byte, 0<=power<=100, to determine the relative
-  power of the kick.
   
-  The function introduces delays into the loop, which obviosuly
-  isn't good. We'll have to figure out a way for it to run without locking
-  the rest of the Arduino system.
-*/
-
-void kick(byte power) {
-  // reset the command byte so that we don't keep kicking.
-  cmd = 0x00;
-  
-  // convert the power from a percentage to a ratio.
-  float relativePower = (power/100.0); 
-  
-  //then perform the kick.
-  kicker.write(90);
-  delay(200);
-  kicker.write(90 - (7 * relativePower));
-  delay(500);
-  kicker.write(90 + (30 * relativePower));
-  delay(200);
-  kicker.write(90 + (15 * relativePower));
-  delay(200);
-  kicker.write(90);
+  // initialise kick functions as they're constant
+  commands[1].functionPtr = &liftKicker;
+  commands[2].functionPtr = &doKick;
+  commands[3].functionPtr = &resetKicker;
 }
-
-/*
-  Decodes the current command byte, and executes the instruction given.
-*/
-
-void decodeCommand() {
-   switch(cmd) {
-     case _FORWARD:
-       moveForward(data);
-       break;
-     case _BACKWARD:
-       moveBackward(data);
-       break;
-     case _STOP:
-       motorAllStop();
-       break;
-     case _KICK:
-       kick(data);
-       break; 
-     case _HEARTBEAT:
-       heartbeat(data);
-       break; 
-     case _GRABBER_OPEN:
-       openGrabber();
-       break;
-     case _GRABBER_CLOSE:
-       closeGrabber();
-       break;
-     case _TURN_LEFT:
-       turnLeft(data);
-       break;
-     case _TURN_TO:
-       turnToHeading(data * 2);
-       break;
-     case _TURN_RIGHT:
-       turnRight(data);
-       break;
-     case _GET_HEADING:
-       returnHeading();
-       break;
-     
-   }
-}
-
-/*
-  Sends a response to a received Hearbeat request by simply
-  returning the request data. We could do something more sophisticated
-  here, but for now it'll probably do.
-*/
-
-void heartbeat(byte _data) {
-  cmd = 0x00;
-  response = _data;
-}
-
-/* Initialises the IMU sensors. Taken (mostly) verbatim from the example program. */
 
 void initSensors()
 {
@@ -247,39 +94,263 @@ void initSensors()
   }
 }
 
-void turnToHeading(short heading) {
-  cmd = 0x00;
-  short currentHeading;
-  short diffHeading;
-  do {
-    currentHeading = getMagOrientation();
-    diffHeading = currentHeading - heading;
-    turnLeft((diffHeading / 2) + 20);
-  } while (abs(diffHeading) > 5);
-  motorAllStop();
+/*
+  Main loop. Runs an endless loop of fetching commands from the serial
+  comms, decoding them, and generally doing the stuff we're supposed to do.
+*/
+
+void loop() {
+  fetchCommand();
+  if (cmd) {
+    decodeCommand();
+  }
+  doStoredCommands();
+  respond();
 }
 
-void returnHeading() {
-  cmd = 0x00;
-  response = (byte) (getMagOrientation() / 2); 
-}
+/*
+  Fetches a single command (command byte + data byte) from
+  the serial stream if there is data available.
+*/
 
-short getMagOrientation() {
-  sensors_event_t mag_event;
-  sensors_vec_t   orientation;
-  
-  /* Calculate the heading using the magnetometer */
-  mag.getEvent(&mag_event);
-  if (dof.magGetOrientation(SENSOR_AXIS_Y, &mag_event, &orientation))
-  {
-    /* 'orientation' should have valid .heading data now */
-    return (short) orientation.heading;
+void fetchCommand() {
+  cmd = 0x00;
+  if (Serial.available() >= 2) {
+    cmd = (char)Serial.read();
+    data = (char)Serial.read();
+    response = 0x00;
   }
 }
 
-short getGyroOrientation() {
-  sensors_event_t event; 
-  gyro.getEvent(&event);
-  
-  return (short) event.gyro.z;
+/*
+  Decodes the current command byte, and executes the instruction given.
+*/
+
+void decodeCommand() {
+  switch(cmd) {
+    // movement commands
+    case _FORWARD:
+      commands[0].millis = millis();
+      commands[0].functionPtr = &moveForward;
+      commands[0].data = data;
+      break; 
+    case _BACKWARD:
+      commands[0].millis = millis();
+      commands[0].functionPtr = &moveBackward;
+      commands[0].data = data;
+      break; 
+    case _STOP:
+      commands[0].millis = millis();
+      commands[0].functionPtr = &driveMotorStop;
+      commands[0].data = data;
+      break;
+
+    // kick commands  
+    case _KICK:
+      current_millis = millis();
+      commands[1].millis = current_millis;
+      commands[2].millis = current_millis + 200;
+      commands[3].millis = current_millis + 400;
+      commands[1].data = commands[2].data = commands[3].data = data;
+      break;
+
+    // grabber commands
+    case _GRABBER_OPEN:
+      current_millis = millis();
+      commands[4].millis = current_millis;
+      commands[4].functionPtr = &startOpenGrabber;
+
+      commands[5].millis = current_millis + 200;
+      commands[5].functionPtr = &slowOpenGrabber;
+
+      commands[6].millis = current_millis + 400;
+      commands[6].functionPtr = &stopGrabber;
+
+      commands[4].data = commands[5].data = commands[6].data = data;
+      break;
+
+    case _GRABBER_CLOSE:
+      current_millis = millis();
+      commands[4].millis = current_millis;
+      commands[4].functionPtr = &startCloseGrabber;
+
+      commands[5].millis = current_millis + 200;
+      commands[5].functionPtr = &slowCloseGrabber;
+
+      commands[6].millis = current_millis + 400;
+      commands[6].functionPtr = &stopGrabber;
+
+      commands[4].data = commands[5].data = commands[6].data = data;
+      break;
+
+     // case _TURN_LEFT:
+     //   turnLeft(data);
+     //   break;
+     // case _TURN_TO:
+     //   turnToHeading(data * 2);
+     //   break;
+     // case _TURN_RIGHT:
+     //   turnRight(data);
+     //   break;
+
+    case _HEARTBEAT:
+      heartbeat(data);
+      break; 
+
+     // case _GET_HEADING:
+     //   returnHeading();
+     //   break;
+     
+   }
 }
+
+void doStoredCommands() {
+  for (short i = 0; i < 7; i++) {
+    if (commands[i].millis && commands[i].millis < millis()) {
+      if (commands[i].functionPtr != NULL) {
+        (*commands[i].functionPtr)(commands[i].data);
+      }
+    }
+  }
+}
+
+void respond() {
+  if (response) {
+    Serial.write(response);
+  }
+  response = 0x00;
+}
+
+void voidCommand(short i) {
+  commands[i].millis = 0;
+}
+
+// Command functions
+// move commands
+void moveForward(byte power) {
+  //check compass and do things
+  motorBackward(LEFT_DRIVE, power);
+  motorForward(RIGHT_DRIVE, power);
+}
+
+void moveBackward(byte power) {
+  //check compass and do things
+  motorForward(LEFT_DRIVE, power); 
+  motorBackward(RIGHT_DRIVE, power);
+}
+
+void driveMotorStop(byte data) {
+  motorStop(LEFT_DRIVE);
+  motorStop(RIGHT_DRIVE);
+  voidCommand(0);
+}
+
+// void turnLeft(byte power) {
+//    motorForward(LEFT_DRIVE, power);
+//    motorForward(RIGHT_DRIVE, power);
+//  }
+ 
+// void turnRight(byte power) {
+//    motorBackward(LEFT_DRIVE, power);
+//    motorBackward(RIGHT_DRIVE, power);
+//  }
+
+//grabber commands
+void startOpenGrabber(byte data) {
+  motorForward(RIGHT_GRABBER, 100);
+  motorBackward(LEFT_GRABBER, 100);
+  voidCommand(4);
+}
+
+void slowOpenGrabber(byte data) {
+  motorForward(RIGHT_GRABBER, 50);
+  motorBackward(LEFT_GRABBER, 50);
+  voidCommand(5);
+}
+
+void startCloseGrabber(byte data) {
+  motorForward(LEFT_GRABBER, 100);
+  motorBackward(RIGHT_GRABBER, 100);
+  voidCommand(4);
+}
+
+void slowCloseGrabber(byte data) {
+  motorForward(LEFT_GRABBER, 50);
+  motorBackward(RIGHT_GRABBER, 50);
+  voidCommand(5);
+}
+
+void stopGrabber(byte data) {
+  motorStop(LEFT_GRABBER);
+  motorStop(RIGHT_GRABBER);
+  voidCommand(6);
+}
+
+//kicker commands
+void liftKicker(byte power) {
+  float relativePower = (power/100.0);
+  kicker.write(90 - (7 * relativePower));
+  voidCommand(1);
+}
+
+void doKick(byte power) {
+  float relativePower = (power/100.0);   
+  kicker.write(90 + (30 * relativePower));
+  voidCommand(2);
+}
+
+void resetKicker(byte data) {
+  kicker.write(90);
+  voidCommand(3);
+}
+
+/*
+  Sends a response to a received Hearbeat request by simply
+  returning the request data. We could do something more sophisticated
+  here, but for now it'll probably do.
+*/
+
+void heartbeat(byte _data) {
+  response = _data;
+}
+
+/* Initialises the IMU sensors. Taken (mostly) verbatim from the example program. */
+
+
+
+// void turnToHeading(short heading) {
+//   cmd = 0x00;
+//   short currentHeading;
+//   short diffHeading;
+//   do {
+//     currentHeading = getMagOrientation();
+//     diffHeading = currentHeading - heading;
+//     turnLeft((diffHeading / 2) + 20);
+//   } while (abs(diffHeading) > 5);
+//   motorAllStop();
+// }
+
+// void returnHeading() {
+//   cmd = 0x00;
+//   response = (byte) (getMagOrientation() / 2); 
+// }
+
+// short getMagOrientation() {
+//   sensors_event_t mag_event;
+//   sensors_vec_t   orientation;
+  
+//   /* Calculate the heading using the magnetometer */
+//   mag.getEvent(&mag_event);
+//   if (dof.magGetOrientation(SENSOR_AXIS_Y, &mag_event, &orientation))
+//   {
+//     /* 'orientation' should have valid .heading data now */
+//     return (short) orientation.heading;
+//   }
+// }
+
+// short getGyroOrientation() {
+//   sensors_event_t event; 
+//   gyro.getEvent(&event);
+  
+//   return (short) event.gyro.z;
+// }
