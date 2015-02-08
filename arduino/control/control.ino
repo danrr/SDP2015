5914ff79d0b580724b96e853bf8c63095bae3938
JP Larsson <jplrssn@gmail.com>
2015-02-08 12:12:01 +0000

    Added (uncompleted) compass stuff.

--- arduino/control/control.ino
+++ arduino/control/control.ino
@@ -2,12 +2,25 @@
   Group 14 Arduino Code
 */
 
-#include "SDPArduino.h"
 #include <Wire.h>
 #include <Servo.h>
 
+#include "SDPArduino.h"
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_9DOF.h>
+#include <Adafruit_L3GD20_U.h>
+
 byte cmd, data, response;
 Servo kicker;
+
+/* Assign a unique ID to the sensors */
+
+Adafruit_9DOF                  dof   = Adafruit_9DOF();
+Adafruit_LSM303_Accel_Unified  accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified    mag   = Adafruit_LSM303_Mag_Unified(30302);
+Adafruit_L3GD20_Unified        gyro = Adafruit_L3GD20_Unified(20);
 
 /*
   Command class bytes. Set to gaming-related stuff
@@ -22,6 +35,17 @@ Servo kicker;
 #define _HEARTBEAT 'L'
 #define _GRABBER_OPEN 'Z'
 #define _GRABBER_CLOSE 'X'
+#define _GET_HEADING 'U'
+#define _TURN_LEFT 'A'
+#define _TURN_RIGHT 'D'
+#define _TURN_TO 'O'
+
+#define LEFT_DRIVE 2
+#define RIGHT_DRIVE 4
+#define BACK_DRIVE 3
+#define LEFT_GRABBER 1
+#define RIGHT_GRABBER 5
+
 
 /*
   Setup function. Performs the standard SDPsetup routine supplied by
@@ -32,6 +56,8 @@ void setup() {
   SDPsetup();
   // attach the kicker servo to pin 5, with 600us to 2400us pulses.
   kicker.attach(5, 600, 2400); 
+  // initialise the IMU
+  initSensors();
 }
 
 /*
@@ -72,8 +98,8 @@ void respond() {
 */
 
 void moveForward(byte power) {
-  motorForward(2, power * 0.7); // 70% scaling on the right motor.
-  motorBackward(4, power);
+  motorBackward(LEFT_DRIVE, power);
+  motorForward(RIGHT_DRIVE, power);
 }
 
 /*
@@ -83,20 +109,38 @@ void moveForward(byte power) {
 */
 
 void moveBackward(byte power) {
-  motorForward(4, power); 
-  motorBackward(2, power * 1); // 100% scaling on the right motor. 
+  motorForward(LEFT_DRIVE, power); 
+  motorBackward(RIGHT_DRIVE, power * 1);
 }
 
+void turnLeft(byte power) {
+   motorForward(LEFT_DRIVE, power);
+   motorForward(RIGHT_DRIVE, power);
+ }
+ 
+void turnRight(byte power) {
+   motorBackward(LEFT_DRIVE, power);
+   motorBackward(RIGHT_DRIVE, power);
+ }
+
 void closeGrabber() {
-  motorForward(4, 100);
-  //delay(500);
-  motorStop(4);
+  cmd = 0x00;
+  motorForward(LEFT_GRABBER, 100);
+  motorBackward(RIGHT_GRABBER, 100);
+  delay(500);
+  motorAllStop();
+  motorStop(LEFT_GRABBER);
+  motorStop(RIGHT_GRABBER);  
 }
 
 void openGrabber() {
-  motorBackward(4, 100);
-  //delay(500);
-  motorStop(4);
+  cmd = 0x00;
+  motorBackward(LEFT_GRABBER, 100);
+  motorForward(RIGHT_GRABBER, 100);
+  delay(500);
+  motorAllStop();
+  motorStop(LEFT_GRABBER);
+  motorStop(RIGHT_GRABBER);  
 }
 
 /*
@@ -150,11 +194,24 @@ void decodeCommand() {
        break; 
      case _GRABBER_OPEN:
        openGrabber();
-     break;
+       break;
      case _GRABBER_CLOSE:
        closeGrabber();
-     break;
-   } 
+       break;
+     case _TURN_LEFT:
+       turnLeft(data);
+       break;
+     case _TURN_TO:
+       turnToHeading(data * 2);
+       break;
+     case _TURN_RIGHT:
+       turnRight(data);
+       break;
+     case _GET_HEADING:
+       returnHeading();
+       break;
+     
+   }
 }
 
 /*
@@ -166,4 +223,63 @@ void decodeCommand() {
 void heartbeat(byte _data) {
   cmd = 0x00;
   response = _data;
+}
+
+/* Initialises the IMU sensors. Taken (mostly) verbatim from the example program. */
+
+void initSensors()
+{
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+  }
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+  }
+  /* Enable auto-ranging */
+  gyro.enableAutoRange(true);
+  
+  /* Initialise the sensor */
+  if(!gyro.begin())
+  {
+    /* There was a problem detecting the L3GD20 ... check your connections */
+  }
+}
+
+void turnToHeading(short heading) {
+  cmd = 0x00;
+  short currentHeading;
+  short diffHeading;
+  do {
+    currentHeading = getMagOrientation();
+    diffHeading = currentHeading - heading;
+    turnLeft((diffHeading / 2) + 20);
+  } while (abs(diffHeading) > 5);
+  motorAllStop();
+}
+
+void returnHeading() {
+  cmd = 0x00;
+  response = (byte) (getMagOrientation() / 2); 
+}
+
+short getMagOrientation() {
+  sensors_event_t mag_event;
+  sensors_vec_t   orientation;
+  
+  /* Calculate the heading using the magnetometer */
+  mag.getEvent(&mag_event);
+  if (dof.magGetOrientation(SENSOR_AXIS_Y, &mag_event, &orientation))
+  {
+    /* 'orientation' should have valid .heading data now */
+    return (short) orientation.heading;
+  }
+}
+
+short getGyroOrientation() {
+  sensors_event_t event; 
+  gyro.getEvent(&event);
+  
+  return (short) event.gyro.z;
 }
