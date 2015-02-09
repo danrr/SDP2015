c667be1b93ffd9ea2fa7a71bfa15b9d6b474fd79
Marton Szeles <s1244474@hikuai.inf.ed.ac.uk>
2015-02-09 20:17:13 +0000

    Modified the high-level 'plan' function slightly specifically for the milestone.

--- milestone2-master/planning/planner.py
+++ milestone2-master/planning/planner.py
@@ -76,39 +76,48 @@ class Planner:
         their_attacker = self._world.their_attacker
         ball = self._world.ball
         if robot == 'defender':
-            # If the ball is in their attacker zone:
-            if self._world.pitch.zones[their_attacker.zone].isInside(ball.x, ball.y):
-                # If the bal is not in the defender's zone, the state should always be 'defend'.
+            # If the ball is in not in our defender zone and it's moving (TODO: refine margin)
+            if ((self._world.pitch.zones[their_attacker.zone].isInside(ball.x, ball.y) == True) or 
+                ((ball.velocity > 2) and ((ball.angle > 2.6) and (ball.angle < 3.7)))):
+                 
+                print "[DEFENDER]: defend!"
+                # If the ball is not in the defender's zone, the state should always be 'defend'.
                 if not self._defender_state == 'defence':
                     self._defender_state = 'defence'
                     self._defender_current_strategy = self.choose_defender_strategy(self._world)
                 return self._defender_current_strategy.generate()
 
-            # We have the ball in our zone, so we grab and pass:
+            # If we have the ball in our zone we grab and pass:
             elif self._world.pitch.zones[our_defender.zone].isInside(ball.x, ball.y):
-                # Check if we should switch from a grabbing to a scoring strategy.
+                
+                # If our task was to grab the ball and the ball is already grabbed, then pass
                 if  self._defender_state == 'grab' and self._defender_current_strategy.current_state == 'GRABBED':
+                    print "[DEFENDER]: we already grabbed the ball, so now pass it!"
                     self._defender_state = 'pass'
                     self._defender_current_strategy = self.choose_defender_strategy(self._world)
 
                 # Check if we should switch from a defence to a grabbing strategy.
                 elif self._defender_state == 'defence':
+                    print "[DEFENDER]: the ball is in our zone we have to grab it"
                     self._defender_state = 'grab'
                     self._defender_current_strategy = self.choose_defender_strategy(self._world)
 
+                # If our task was to pass the ball and we're done
                 elif self._defender_state == 'pass' and self._defender_current_strategy.current_state == 'FINISHED':
+                    print "[DEFENDER]: our task was to pass the ball and we're done"
                     self._defender_state = 'grab'
                     self._defender_current_strategy = self.choose_defender_strategy(self._world)
 
                 return self._defender_current_strategy.generate()
             # Otherwise, chillax:
             else:
-
+                print "[DEFENDER]: do nothing"
                 return do_nothing()
 
         else:
             # If the ball is in their defender zone we defend:
             if self._world.pitch.zones[their_defender.zone].isInside(ball.x, ball.y):
+                print "[ATTACKER]: the ball is in their defender zone so just defend"
                 if not self._attacker_state == 'defence':
                     self._attacker_state = 'defence'
                     self._attacker_current_strategy = self.choose_attacker_strategy(self._world)
@@ -119,6 +128,7 @@ class Planner:
 
                 # Check if we should switch from a grabbing to a scoring strategy.
                 if self._attacker_state == 'grab' and self._attacker_current_strategy.current_state == 'GRABBED':
+                    print "[ATTACKER]: we grabbed the ball so go ahead and score"
                     self._attacker_state = 'score'
                     self._attacker_current_strategy = self.choose_attacker_strategy(self._world)
 
@@ -126,28 +136,34 @@ class Planner:
                     # Switch to careful mode if the ball is too close to the wall.
                     if abs(self._world.ball.y - self._world.pitch.height) < 0 or abs(self._world.ball.y) < 0:
                         if isinstance(self._attacker_current_strategy, AttackerGrab):
+                            print "[ATTACKER]: grab carefully"
                             self._attacker_current_strategy = AttackerGrabCareful(self._world)
                     else:
                         if isinstance(self._attacker_current_strategy, AttackerGrabCareful):
+                            print "[ATTACKER]: grab"
                             self._attacker_current_strategy = AttackerGrab(self._world)
 
                 # Check if we should switch from a defence to a grabbing strategy.
                 elif self._attacker_state in ['defence', 'catch'] :
                     self._attacker_state = 'grab'
                     self._attacker_current_strategy = self.choose_attacker_strategy(self._world)
+                    print "[ATTACKER]: start grabbing strategy"
 
                 elif self._attacker_state == 'score' and self._attacker_current_strategy.current_state == 'FINISHED':
                     self._attacker_state = 'grab'
                     self._attacker_current_strategy = self.choose_attacker_strategy(self._world)
+                    print "[ATTACKER]: we finished kicking the ball"
 
                 return self._attacker_current_strategy.generate()
             # If the ball is in our defender zone, prepare to catch the passed ball:
             elif self._world.pitch.zones[our_defender.zone].isInside(ball.x, ball.y) or \
                  self._attacker_state == 'catch':
+                print "[ATTACKER]: prepare to catch the ball passed from the defender"
                  # self._world.pitch.zones[their_attacker.zone].isInside(ball.x, ball.y):
                 if not self._attacker_state == 'catch':
                     self._attacker_state = 'catch'
                     self._attacker_current_strategy = self.choose_attacker_strategy(self._world)
                 return self._attacker_current_strategy.generate()
             else:
+                print "[ATTACKER]: motor speed on 0"
                 return calculate_motor_speed(0, 0)
