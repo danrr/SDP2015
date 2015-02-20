c6cb0f708bee23260046c597ffae41615f7c31f0
Dan Ristea <s1244166@sms.ed.ac.uk>
2015-02-20 13:25:58 +0000

    PEP 8

--- planning/utilities.py
+++ planning/utilities.py
@@ -2,30 +2,28 @@ from math import tan, pi, hypot, log
 from models import Robot
 
 DISTANCE_MATCH_THRESHOLD = 13
-ANGLE_MATCH_THRESHOLD = pi/5
-BALL_ANGLE_THRESHOLD = pi/10
+ANGLE_MATCH_THRESHOLD = pi / 5
+BALL_ANGLE_THRESHOLD = pi / 10
 MAX_DISPLACEMENT_SPEED = 690
 MAX_ANGLE_SPEED = 50
 BALL_VELOCITY = 3
 
 
 def is_shot_blocked(world, our_robot, their_robot):
-    '''
+    """
     Checks if our robot could shoot past their robot
-    '''
+    """
     predicted_y = predict_y_intersection(
         world, their_robot.x, our_robot, full_width=True, bounce=True)
     if predicted_y is None:
         return True
-    print '##########', predicted_y, their_robot.y, their_robot.length
-    print abs(predicted_y - their_robot.y) < their_robot.length
     return abs(predicted_y - their_robot.y) < their_robot.length
 
 
 def is_attacker_shot_blocked(world, our_attacker, their_defender):
-    '''
+    """
     Checks if our attacker would score if it would immediately turn and shoot.
-    '''
+    """
 
     # Acceptable distance that the opponent defender can be relative to our
     # shooting position in order for us to have a clear shot.
@@ -55,91 +53,94 @@ def can_score(world, our_robot, their_goal, turn=0):
 
     return goal_posts[0][1] < predicted_y < goal_posts[1][1]
 
+
 def predict_y_intersection(world, predict_for_x, robot, full_width=False, bounce=False):
-        '''
-        Predicts the (x, y) coordinates of the ball shot by the robot
-        Corrects them if it's out of the bottom_y - top_y range.
-        If bounce is set to True, predicts for a bounced shot
-        Returns None if the robot is facing the wrong direction.
-        '''
-        x = robot.x
-        y = robot.y
-        top_y = world._pitch.height - 60 if full_width else world.our_goal.y + (world.our_goal.width/2) - 30
-        bottom_y = 60 if full_width else world.our_goal.y - (world.our_goal.width/2) + 30
-        angle = robot.angle
-        if (robot.x < predict_for_x and not (pi/2 < angle < 3*pi/2)) or (robot.x > predict_for_x and (3*pi/2 > angle > pi/2)):
-            if bounce:
-                if not (0 <= (y + tan(angle) * (predict_for_x - x)) <= world._pitch.height):
-                    bounce_pos = 'top' if (y + tan(angle) * (predict_for_x - x)) > world._pitch.height else 'bottom'
-                    x += (world._pitch.height - y) / tan(angle) if bounce_pos == 'top' else (0 - y) / tan(angle)
-                    y = world._pitch.height if bounce_pos == 'top' else 0
-                    angle = (-angle) % (2*pi)
-            predicted_y = (y + tan(angle) * (predict_for_x - x))
-            # Correcting the y coordinate to the closest y coordinate on the goal line:
-            if predicted_y > top_y:
-                return top_y
-            elif predicted_y < bottom_y:
-                return bottom_y
-            return predicted_y
-        else:
-            return None
+    """
+    Predicts the (x, y) coordinates of the ball shot by the robot
+    Corrects them if it's out of the bottom_y - top_y range.
+    If bounce is set to True, predicts for a bounced shot
+    Returns None if the robot is facing the wrong direction.
+    """
+    x = robot.x
+    y = robot.y
+    top_y = world._pitch.height - 60 if full_width else world.our_goal.y + (world.our_goal.width / 2) - 30
+    bottom_y = 60 if full_width else world.our_goal.y - (world.our_goal.width / 2) + 30
+    angle = robot.angle
+    if (robot.x < predict_for_x and not (pi / 2 < angle < 3 * pi / 2)) or \
+            (robot.x > predict_for_x and (3 * pi / 2 > angle > pi / 2)):
+        if bounce:
+            if not (0 <= (y + tan(angle) * (predict_for_x - x)) <= world._pitch.height):
+                bounce_pos = 'top' if (y + tan(angle) * (predict_for_x - x)) > world._pitch.height else 'bottom'
+                x += (world._pitch.height - y) / tan(angle) if bounce_pos == 'top' else (0 - y) / tan(angle)
+                y = world._pitch.height if bounce_pos == 'top' else 0
+                angle = (-angle) % (2 * pi)
+        predicted_y = (y + tan(angle) * (predict_for_x - x))
+        # Correcting the y coordinate to the closest y coordinate on the goal line:
+        if predicted_y > top_y:
+            return top_y
+        elif predicted_y < bottom_y:
+            return bottom_y
+        return predicted_y
+    else:
+        return None
 
 
 def grab_ball_center():
-    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber' : 0, 'kick':0}
+    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber': 0, 'kick': 0}
+
 
 def grab_ball_right():
-    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber' : 1, 'kick':0}
+    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber': 1, 'kick': 0}
+
 
 def grab_ball_left():
-    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber' : 2, 'kick':0}
+    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber': 2, 'kick': 0}
+
 
 def kick_ball(power):
-    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber' : -1, 'kick':power}
+    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber': -1, 'kick': power}
 
 
 def open_catcher():
-    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber' : 3, 'kick':0}
+    return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber': 3, 'kick': 0}
 
 
 def turn_shoot(orientation, power):
-    return {'move': 0, 'strafe': 0, 'angle': orientation, 'grabber' : -1, 'kick':power}
+    return {'move': 0, 'strafe': 0, 'angle': orientation, 'grabber': -1, 'kick': power}
 
 
 def has_matched(robot, x=None, y=None, angle=None,
                 angle_threshold=ANGLE_MATCH_THRESHOLD, distance_threshold=DISTANCE_MATCH_THRESHOLD):
     dist_matched = True
     angle_matched = True
-    if not(x is None and y is None):
+    if not (x is None and y is None):
         dist_matched = hypot(robot.x - x, robot.y - y) < distance_threshold
-    if not(angle is None):
+    if not (angle is None):
         angle_matched = abs(angle) < angle_threshold
     return dist_matched and angle_matched
 
 
 def move(displacement, angle, strafe_ok=False, backwards_ok=False, careful=False):
-    '''
+    """
     Move in a heading given by "angle" for a distance "displacement". If grabbing the ball, strafe_ok and backwards_ok
     should be false so the robot can orientate itself to face the ball. Otherwise it should be able to strafe or reverse
     in order to reach it's destination quickly
-    '''
-    print('Angle: '+ str(angle*180/pi))
+    """
     moving_backwards = False
     moving_sideways = False
     angle_thresh = BALL_ANGLE_THRESHOLD if careful else ANGLE_MATCH_THRESHOLD
 
-
     # If heading is greater than 135 degrees the robot should go backwards, therefore the heading needs to be adjusted
     # by 180 degrees
 
-    if backwards_ok and abs(angle) > (3*pi)/4:
+    if backwards_ok and abs(angle) > (3 * pi) / 4:
         angle = (-pi + angle) if angle > 0 else (pi + angle)
         moving_backwards = True
 
     # If heading is between 135 and 45 degrees the robot should strafe, therefore the heading needs to be adjusted by
     # 90 degrees
-    if strafe_ok and abs(angle) <= (3*pi)/4 and abs(angle) >= pi/4:
-        angle = angle - (pi/2) if angle > 0 else angle + (pi/2)
+    if strafe_ok and (3 * pi) / 4 >= abs(angle) >= pi / 4:
+        angle = angle - (pi / 2) if angle > 0 else angle + (pi / 2)
         moving_sideways = True
         if angle < 0:
             moving_backwards = True
@@ -149,25 +150,24 @@ def move(displacement, angle, strafe_ok=False, backwards_ok=False, careful=False
             return do_nothing()
 
         elif abs(angle) > angle_thresh:
-            angle = int(((angle/pi)* 180)/2)
-            return {'move': 0, 'strafe': 0, 'angle': angle, 'grabber' : -1, 'kick':0}
+            angle = int(((angle / pi) * 180) / 2)
+            return {'move': 0, 'strafe': 0, 'angle': angle, 'grabber': -1, 'kick': 0}
 
         else:
             speed = 40
-            # print 'DISP:', displacement
             if not moving_sideways:
-                return {'move': speed, 'strafe': 0, 'angle': 0, 'grabber' : -1, 'kick':0}
+                return {'move': speed, 'strafe': 0, 'angle': 0, 'grabber': -1, 'kick': 0}
             else:
-                return {'move': 0, 'strafe': speed, 'angle': 0, 'grabber' : -1, 'kick':0}
+                return {'move': 0, 'strafe': speed, 'angle': 0, 'grabber': -1, 'kick': 0}
 
     else:
 
         if abs(angle) > angle_thresh:
-            angle = int(((angle/pi) * 180)/2)
-            return {'move': 0, 'strafe': 0, 'angle': angle, 'grabber' : -1, 'kick':0}
+            angle = int(((angle / pi) * 180) / 2)
+            return {'move': 0, 'strafe': 0, 'angle': angle, 'grabber': -1, 'kick': 0}
 
         else:
-            return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber' : -1, 'kick':0}
+            return {'move': 0, 'strafe': 0, 'angle': 0, 'grabber': -1, 'kick': 0}
 
 
 def do_nothing():
